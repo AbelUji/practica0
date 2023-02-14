79522b9dc8893589203f0c50fc139a08539c9315
Abel <al415746@uji.es>
2023-02-14 10:48:21 +0100

    Clase test
    test clase hecho

--- src/main/java/principal/Estadistica.java
+++ src/main/java/principal/Estadistica.java
@@ -1,4 +1,41 @@
 package principal;
 
+import java.util.ArrayList;
+
 public class Estadistica {
+
+    public float media(ArrayList<Float> lista){
+
+        if(lista.size() == 0 ){
+            return 0;
+        }
+        float media = 0;
+        for(int i = 0;i<lista.size();i++){
+            media += lista.get(i);
+        }
+        return media/lista.size();
+    }
+
+    public float varianza(ArrayList<Float> lista){
+        if(lista.size() == 0 ){
+            return 0;
+        }
+        float media = media(lista);
+        float varianza = 0;
+        for(int i = 0; i<lista.size();i++){
+            varianza += Math.pow(lista.get(i)-media,2);
+        }
+        return varianza/lista.size();
+    }
+
+    public float desviacionEstandar(ArrayList<Float> lista){
+        if(lista.size() == 0 ){
+            return 0;
+        }
+        float varianza = varianza(lista);
+        return (float) Math.sqrt(varianza);
+
+    }
+
+
 }
---
+++ src/main/java/principal/EstadisticaTest.java
@@ -0,0 +1,23 @@
+package principal;
+
+import org.junit.jupiter.api.DisplayName;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class EstadisticaTest {
+
+    @org.junit.jupiter.api.Test
+    @DisplayName("Test de la media")
+    void media() {
+    }
+
+    @org.junit.jupiter.api.Test
+    @DisplayName("Test de la varianza")
+    void varianza() {
+    }
+
+    @org.junit.jupiter.api.Test
+    @DisplayName("Test de la desviacion Estandar    ")
+    void desviacionEstandar() {
+    }
+}
